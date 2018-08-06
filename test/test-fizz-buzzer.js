bfeb81f9f2d280e8e89edeec69ad1d23b287f2af
Bob Becket <bobbybecket@gmail.com>
2018-08-06 11:34:51 -0700

    initial commit

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,61 @@
+// import chai, declare expect variable
+const expect = require('chai').expect;
+
+// import fizzBuzzer
+const fizzBuzzer = require('../fizzBuzzer');
+
+// unit tests for our `fizzBuzzer` function
+describe('fizzBuzzer', function() {
+
+  // test the normal case
+  it('should return fizzBuzz for divisible by 15', function() {
+    const normalCases = [
+      {a: 15, expected: 'fizz-buzz'},
+      {a: 30, expected: 'fizz-buzz'}
+    ];
+
+    normalCases.forEach(function(input) {
+      const answer = fizzBuzzer(input.a);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+
+  it('should return fizzBuzz for divisible by 5', function() {
+    const normalCases = [
+      {a: 5, expected: 'buzz'},
+      {a: 10, expected: 'buzz'}
+    ];
+
+    normalCases.forEach(function(input) {
+      const answer = fizzBuzzer(input.a);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+
+  it('should return fizzBuzz for divisible by 3', function() {
+    const normalCases = [
+      {a: 3, expected: 'fizz'},
+      {a: 6, expected: 'fizz'}
+    ];
+
+    normalCases.forEach(function(input) {
+      const answer = fizzBuzzer(input.a);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+
+  it('should raise error if args not number', function() {
+    // range of bad inputs where not both are numbers
+    const badInputs = [
+      ['a'],
+      ['1'],
+      [false]
+    ];
+    // prove that an error is raised for bad inputs
+    badInputs.forEach(function(input) {
+      expect(function() {
+        fizzBuzzer(input[0]);
+      }).to.throw(Error);
+    });
+  });
+});
